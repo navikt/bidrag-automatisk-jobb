61743555751c27b94f07ccd4319ed0267d92aa8d
Simen Høston <simen.naper.hoston@webstep.no>
2024-11-27 11:18:22 +0100

    Returnerer responsentity

--- src/main/kotlin/no/nav/bidrag/aldersjustering/controller/AldersjusteringController.kt
+++ src/main/kotlin/no/nav/bidrag/aldersjustering/controller/AldersjusteringController.kt
@@ -4,9 +4,9 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
-import no.nav.bidrag.aldersjustering.persistence.entity.Barn
 import no.nav.bidrag.aldersjustering.service.AldersjusteringService
 import no.nav.security.token.support.core.api.Protected
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -25,9 +25,11 @@ class AldersjusteringController(
         value = [
             ApiResponse(
                 responseCode = "200",
-                description = "Returnerer map over barn som fyller 6, 11 eller 15 inneværende år.",
+                description = "Returnerer map over barn som fyller 6, 11 eller 15 for gitt år år.",
             ),
         ],
     )
-    fun hentBarnSomSkalAldersjusteres(): Map<Int, List<Barn>> = aldersjusteringService.hentAlleBarnSomSkalAldersjusteresForÅr(2021)
+    fun hentBarnSomSkalAldersjusteres(år: Int): ResponseEntity<Any> {
+        return ResponseEntity.ok(aldersjusteringService.hentAlleBarnSomSkalAldersjusteresForÅr(år))
+    }
 }
