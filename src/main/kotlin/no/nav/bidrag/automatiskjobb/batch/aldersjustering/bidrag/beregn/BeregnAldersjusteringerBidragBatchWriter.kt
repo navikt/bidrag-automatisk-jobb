c8175c6bb171fd9ad3f156ea689790d4f447a278
Simen Høston <simen.naper.hoston@webstep.no>
2025-04-25 18:49:12 +0200

    Legger til nytt batch-jobb for aldersjustering av bidrag

--- src/main/kotlin/no/nav/bidrag/automatiskjobb/batch/aldersjustering/bidrag/beregn/BeregnAldersjusteringerBidragBatchWriter.kt
+++ src/main/kotlin/no/nav/bidrag/automatiskjobb/batch/aldersjustering/bidrag/beregn/BeregnAldersjusteringerBidragBatchWriter.kt
@@ -1,37 +1,33 @@
-package no.nav.bidrag.automatiskjobb.batch.bidrag
+package no.nav.bidrag.automatiskjobb.batch.aldersjustering.bidrag.beregn
 
-import no.nav.bidrag.automatiskjobb.persistence.entity.Barn
+import no.nav.bidrag.automatiskjobb.persistence.entity.Aldersjustering
 import no.nav.bidrag.automatiskjobb.service.AldersjusteringService
 import no.nav.bidrag.domene.enums.vedtak.Stønadstype
 import org.springframework.batch.core.StepExecution
 import org.springframework.batch.core.annotation.BeforeStep
+import org.springframework.batch.core.configuration.annotation.StepScope
 import org.springframework.batch.item.Chunk
 import org.springframework.batch.item.ItemWriter
 import org.springframework.stereotype.Component
 
 @Component
-class AldersjusteringBidragBatchWriter(
+@StepScope
+class BeregnAldersjusteringerBidragBatchWriter(
     private val aldersjusteringService: AldersjusteringService,
-) : ItemWriter<Barn?> {
-    private var jobExecutionId: String = ""
-    private var år: Long? = -1
+) : ItemWriter<Aldersjustering> {
     private var simuler: Boolean = true
 
     @BeforeStep
     fun beforeStep(stepExecution: StepExecution) {
-        jobExecutionId = stepExecution.jobExecutionId.toString()
-        år = stepExecution.jobParameters.getLong("år")
         simuler = stepExecution.jobParameters.getString("simuler").toBoolean()
     }
 
-    override fun write(chunk: Chunk<out Barn>) {
-        chunk.forEach { barn ->
-            aldersjusteringService.utførAldersjusteringForBarn(
+    override fun write(chunk: Chunk<out Aldersjustering>) {
+        chunk.forEach { aldersjustering ->
+            aldersjusteringService.utførAldersjustering(
+                aldersjustering = aldersjustering,
                 stønadstype = Stønadstype.BIDRAG,
-                barn = barn,
-                år = år!!.toInt(),
-                batchId = jobExecutionId,
-                simuler = simuler,
+                simuler,
             )
         }
     }
