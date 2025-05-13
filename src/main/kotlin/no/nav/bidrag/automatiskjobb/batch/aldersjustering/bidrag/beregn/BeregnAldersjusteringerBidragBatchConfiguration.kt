cf51247b2679ab61159ba3363eeda3eba8509e6a
Ugur Alpay Cenar <cenar93@gmail.com>
2025-05-13 20:15:43 +0200

    Endre beregn til å bruke concurrent løsningen

--- src/main/kotlin/no/nav/bidrag/automatiskjobb/batch/aldersjustering/bidrag/beregn/BeregnAldersjusteringerBidragBatchConfiguration.kt
+++ src/main/kotlin/no/nav/bidrag/automatiskjobb/batch/aldersjustering/bidrag/beregn/BeregnAldersjusteringerBidragBatchConfiguration.kt
@@ -1,16 +1,18 @@
 package no.nav.bidrag.automatiskjobb.batch.aldersjustering.bidrag.beregn
 
 import no.nav.bidrag.automatiskjobb.batch.BatchCompletionNotificationListener
-import no.nav.bidrag.automatiskjobb.batch.common.ModuloPartitioner
+import no.nav.bidrag.automatiskjobb.batch.aldersjustering.bidrag.opprett.OpprettAldersjusteringerBidragBatchConfiguration
 import no.nav.bidrag.automatiskjobb.persistence.entity.Aldersjustering
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
 import org.springframework.batch.core.job.builder.JobBuilder
 import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.task.SimpleAsyncTaskExecutor
+import org.springframework.core.task.TaskExecutor
 import org.springframework.transaction.PlatformTransactionManager
 
 @Configuration
@@ -21,18 +23,25 @@ class BeregnAldersjusteringerBidragBatchConfiguration {
     }
 
     @Bean
+    fun beregnAldersjusteringExecutor(): TaskExecutor? =
+        SimpleAsyncTaskExecutor("beregn_aldersjustering").apply {
+            concurrencyLimit = OpprettAldersjusteringerBidragBatchConfiguration.GRID_SIZE
+        }
+
+    @Bean
     fun beregnAldersjusteringerBidragJob(
         jobRepository: JobRepository,
-        partitionedBeregnAldersjusteringerBidragStep: Step,
+        beregnAldersjusteringerBidragStep: Step,
         listener: BatchCompletionNotificationListener,
     ): Job =
         JobBuilder("beregnAldersjusteringerBidragJob", jobRepository)
             .listener(listener)
-            .start(partitionedBeregnAldersjusteringerBidragStep)
+            .start(beregnAldersjusteringerBidragStep)
             .build()
 
     @Bean
     fun beregnAldersjusteringerBidragStep(
+        @Qualifier("beregnAldersjusteringExecutor") taskExecutor: TaskExecutor,
         jobRepository: JobRepository,
         transactionManager: PlatformTransactionManager,
         beregnAldersjusteringerBidragBatchReader: BeregnAldersjusteringerBidragBatchReader,
@@ -41,21 +50,7 @@ class BeregnAldersjusteringerBidragBatchConfiguration {
         StepBuilder("beregnAldersjusteringerBidragStep", jobRepository)
             .chunk<Aldersjustering, Aldersjustering>(CHUNK_SIZE, transactionManager)
             .reader(beregnAldersjusteringerBidragBatchReader)
-            .processor(beregnAldersjusteringerBidragBatchReader)
             .writer(beregnAldersjusteringerBidragBatchWriter)
-            .build()
-
-    @Bean
-    fun partitionedBeregnAldersjusteringerBidragStep(
-        jobRepository: JobRepository,
-        transactionManager: PlatformTransactionManager,
-        beregnAldersjusteringerBidragStep: Step,
-        moduloPartitioner: ModuloPartitioner,
-    ): Step =
-        StepBuilder("partitionedStep", jobRepository)
-            .partitioner("beregnAldersjusteringerBidragStep", moduloPartitioner)
-            .step(beregnAldersjusteringerBidragStep)
-            .gridSize(GRID_SIZE)
-            .taskExecutor(SimpleAsyncTaskExecutor())
+            .taskExecutor(taskExecutor)
             .build()
 }
--- src/main/kotlin/no/nav/bidrag/automatiskjobb/batch/aldersjustering/bidrag/beregn/BeregnAldersjusteringerBidragBatchReader.kt
+++ src/main/kotlin/no/nav/bidrag/automatiskjobb/batch/aldersjustering/bidrag/beregn/BeregnAldersjusteringerBidragBatchReader.kt
@@ -1,24 +1,19 @@
 package no.nav.bidrag.automatiskjobb.batch.aldersjustering.bidrag.beregn
 
-import no.nav.bidrag.automatiskjobb.batch.common.ModuloItemProcessor
 import no.nav.bidrag.automatiskjobb.persistence.entity.Aldersjustering
 import no.nav.bidrag.automatiskjobb.persistence.entity.Status
 import no.nav.bidrag.automatiskjobb.persistence.repository.AldersjusteringRepository
 import org.springframework.batch.core.configuration.annotation.StepScope
-import org.springframework.beans.factory.annotation.Value
+import org.springframework.batch.item.data.RepositoryItemReader
 import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Component
-import org.springframework.transaction.PlatformTransactionManager
 import java.util.Collections
 
 @Component
 @StepScope
 class BeregnAldersjusteringerBidragBatchReader(
     aldersjusteringRepository: AldersjusteringRepository,
-    @Value("#{stepExecutionContext['partitionNumber']}") private val partitionNumber: Int?,
-    @Value("#{stepExecutionContext['gridSize']}") private val gridSize: Int?,
-    transactionManager: PlatformTransactionManager,
-) : ModuloItemProcessor<Aldersjustering>(partitionNumber, gridSize, transactionManager) {
+) : RepositoryItemReader<Aldersjustering>() {
     init {
         this.setRepository(aldersjusteringRepository)
         this.setMethodName("finnForFlereStatuser")
