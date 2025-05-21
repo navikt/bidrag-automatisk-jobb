7a9d827b2ab2a73bc094971bb8958f6ee0846fcd
Ugur Alpay Cenar <cenar93@gmail.com>
2025-05-21 20:42:00 +0200

    Reduser grid og chunk size

--- src/main/kotlin/no/nav/bidrag/automatiskjobb/batch/BatchConfiguration.kt
+++ src/main/kotlin/no/nav/bidrag/automatiskjobb/batch/BatchConfiguration.kt
@@ -8,9 +8,9 @@ import org.springframework.core.task.TaskExecutor
 @Configuration
 class BatchConfiguration {
     companion object {
-        const val CHUNK_SIZE = 500
-        const val PAGE_SIZE = 500
-        const val GRID_SIZE = 10
+        const val CHUNK_SIZE = 100
+        const val PAGE_SIZE = 100
+        const val GRID_SIZE = 5
     }
 
     @Bean
