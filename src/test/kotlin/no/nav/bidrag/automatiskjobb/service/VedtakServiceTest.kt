2c2cd0c4c86316a836e86034227372d7ea04a568
Ugur Alpay Cenar <cenar93@gmail.com>
2025-05-21 10:47:24 +0200

    Bugfix identutils, bruk metode fra felles (#91)
    * Bugfix identutils, bruk metode fra felles
    
    * Bugfix identutils, bruk metode fra felles

--- src/test/kotlin/no/nav/bidrag/automatiskjobb/service/VedtakServiceTest.kt
+++ src/test/kotlin/no/nav/bidrag/automatiskjobb/service/VedtakServiceTest.kt
@@ -12,7 +12,7 @@ import io.mockk.verify
 import no.nav.bidrag.automatiskjobb.consumer.BidragPersonConsumer
 import no.nav.bidrag.automatiskjobb.persistence.entity.Barn
 import no.nav.bidrag.automatiskjobb.persistence.repository.BarnRepository
-import no.nav.bidrag.automatiskjobb.utils.IdentUtils
+import no.nav.bidrag.commons.util.IdentUtils
 import no.nav.bidrag.commons.util.PersonidentGenerator
 import no.nav.bidrag.domene.enums.vedtak.Beslutningstype
 import no.nav.bidrag.domene.enums.vedtak.Innkrevingstype
