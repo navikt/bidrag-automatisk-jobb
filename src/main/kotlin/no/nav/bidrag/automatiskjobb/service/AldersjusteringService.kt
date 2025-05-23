fde280def0b9e4bcbcad9349c470a34df4752a2d
Ugur Alpay Cenar <cenar93@gmail.com>
2025-05-23 14:18:26 +0200

    Aldersjustering for de som er avvist

--- src/main/kotlin/no/nav/bidrag/automatiskjobb/service/AldersjusteringService.kt
+++ src/main/kotlin/no/nav/bidrag/automatiskjobb/service/AldersjusteringService.kt
@@ -33,7 +33,6 @@ import no.nav.bidrag.transport.behandling.vedtak.request.OpprettVedtakRequestDto
 import org.springframework.data.domain.Pageable
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.client.HttpStatusCodeException
 import java.sql.Timestamp
 
@@ -192,7 +191,6 @@ class AldersjusteringService(
         }
     }
 
-    @Transactional
     fun utførAldersjustering(
         aldersjustering: Aldersjustering,
         stønadstype: Stønadstype,
