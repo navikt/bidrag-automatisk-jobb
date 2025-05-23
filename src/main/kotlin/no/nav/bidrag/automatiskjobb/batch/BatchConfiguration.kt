471fe0e2e7409735e013c425b4c57ad36e36c6a5
Ugur Alpay Cenar <cenar93@gmail.com>
2025-05-23 11:41:54 +0200

    Aldersjustering for de som er avvist

--- src/main/kotlin/no/nav/bidrag/automatiskjobb/batch/BatchConfiguration.kt
+++ src/main/kotlin/no/nav/bidrag/automatiskjobb/batch/BatchConfiguration.kt
@@ -2,20 +2,21 @@ package no.nav.bidrag.automatiskjobb.batch
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.task.SimpleAsyncTaskExecutor
 import org.springframework.core.task.TaskExecutor
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 
 @Configuration
 class BatchConfiguration {
     companion object {
-        const val CHUNK_SIZE = 500
-        const val PAGE_SIZE = 500
+        const val CHUNK_SIZE = 100
+        const val PAGE_SIZE = 100
         const val GRID_SIZE = 10
     }
 
     @Bean
     fun batchTaskExecutor(): TaskExecutor? =
-        SimpleAsyncTaskExecutor("batch").apply {
-            concurrencyLimit = GRID_SIZE
+        ThreadPoolTaskExecutor().apply {
+            setVirtualThreads(true)
+            maxPoolSize = GRID_SIZE
         }
 }
