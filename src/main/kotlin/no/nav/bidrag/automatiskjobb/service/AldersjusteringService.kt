0d1fbf2439146915282a3c891cf3c48cb0cf8b88
Simen Høston <simen.naper.hoston@webstep.no>
2025-06-03 11:12:25 +0200

    Endre hentAntallBarnSomSkalAldersjusteresForÅr til å returnere en Map med antall barn per alder

--- src/main/kotlin/no/nav/bidrag/automatiskjobb/service/AldersjusteringService.kt
+++ src/main/kotlin/no/nav/bidrag/automatiskjobb/service/AldersjusteringService.kt
@@ -418,12 +418,12 @@ class AldersjusteringService(
         return result
     }
 
-    fun hentAntallBarnSomSkalAldersjusteresForÅr(år: Int): Int =
+    fun hentAntallBarnSomSkalAldersjusteresForÅr(år: Int): Map<Int, Int> =
         barnRepository
             .finnBarnSomSkalAldersjusteresForÅr(år)
             .filter { it.fødselsdato != null }
             .groupBy { år - it.fødselsdato!!.year }
-            .size
+            .mapValues { it.value.size }
 
     fun hentAldersjustering(id: Int): Aldersjustering? = alderjusteringRepository.findById(id).orElseGet { null }
 
