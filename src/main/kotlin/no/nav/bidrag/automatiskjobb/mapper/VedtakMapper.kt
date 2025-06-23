37b840c9612f609775839fb7421172b21b6c8f59
Ugur Alpay Cenar <cenar93@gmail.com>
2025-06-23 11:44:53 +0200

    Legge til indeksår i stønadsendring (#112)
    * Legge til indeksår i stønadsendring
    
    * Legge til indeksår i stønadsendring
    
    * Legge til indeksår i stønadsendring

--- src/main/kotlin/no/nav/bidrag/automatiskjobb/mapper/VedtakMapper.kt
+++ src/main/kotlin/no/nav/bidrag/automatiskjobb/mapper/VedtakMapper.kt
@@ -96,6 +96,11 @@ class VedtakMapper(
                             grunnlagReferanseListe = listOf(grunnlagAldersjustering.referanse),
                             innkreving = Innkrevingstype.MED_INNKREVING,
                             sisteVedtaksid = vedtakService.finnSisteVedtaksid(stønad),
+                            førsteIndeksreguleringsår =
+                                maxOf(
+                                    aldersjustering.aldersjusteresForÅr + 1,
+                                    YearMonth.now().year + 1,
+                                ),
                             periodeListe =
                                 resultat.beregnetBarnebidragPeriodeListe.map {
                                     OpprettPeriodeRequestDto(
