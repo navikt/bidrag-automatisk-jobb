9f6ca081b478bf7e42aeebb257aebcfdb2e5c999
= <=>
2025-11-19 15:35:15 +0100

    Endrer beskrivelse for oppgave knyttet til revurdering av forskudd

--- src/main/kotlin/no/nav/bidrag/automatiskjobb/utils/OppgaveExtensions.kt
+++ src/main/kotlin/no/nav/bidrag/automatiskjobb/utils/OppgaveExtensions.kt
@@ -5,16 +5,17 @@ import no.nav.bidrag.domene.enums.vedtak.Engangsbeløptype
 import no.nav.bidrag.domene.enums.vedtak.Stønadstype
 import no.nav.bidrag.transport.behandling.vedtak.VedtakHendelse
 
-val revurderForskuddBeskrivelse = "Revurder forskudd basert på inntekt fra nytt vedtak om barnebidrag."
-val revurderForskuddBeskrivelseSærbidrag = "Revurder forskudd basert på inntekt fra nytt vedtak om særbidrag."
-val revurderForskuddBeskrivelseAdresseendring =
+const val revurderForskuddBeskrivelse = "Revurder forskudd basert på inntekt fra nytt vedtak om barnebidrag."
+const val revurderForskuddBeskrivelseSærbidrag = "Revurder forskudd basert på inntekt fra nytt vedtak om særbidrag."
+const val revurderForskuddBeskrivelseAdresseendring =
     "Barnet er ikke lenger folkeregistrert på samme adresse som bidragsmottaker, og forskuddet må revurderes."
-val oppgaveAldersjusteringBeskrivelse =
+const val oppgaveAldersjusteringBeskrivelse =
     "Automatisk aldersjustering av bidrag ble ikke foretatt. " +
         "Det må kontrolleres om saken skal aldersjusteres manuelt. Årsak til at saken ikke ble aldersjustert: {}"
-val oppgaveTilbakekrevingForskudd =
-    "Det er behov for manuell behandling av tilbakekreving av forskudd etter " +
-        "atomatisk revurdering av forskuddet har satt ned forskuddet."
+const val oppgaveTilbakekrevingForskudd =
+    "Forskudd har blitt automatisk revurdert og satt ned. " +
+            "Det har blitt utbetalt forskudd i løpet av siste 3 måneder. " +
+            "Vurder feilutbetaling og eventuell tilbakekreving."
 
 fun VedtakHendelse.erForskudd() = stønadsendringListe?.any { it.type == Stønadstype.FORSKUDD } == true
 
