f1e3feee554fe347cbee938fb0ba9096f87ecb58
Simen Høston <simen.naper.hoston@webstep.no>
2026-01-08 08:26:44 +0100

    ktlint

--- src/main/kotlin/no/nav/bidrag/automatiskjobb/persistence/entity/RevurderingForskudd.kt
+++ src/main/kotlin/no/nav/bidrag/automatiskjobb/persistence/entity/RevurderingForskudd.kt
@@ -54,14 +54,14 @@ data class RevurderingForskudd(
 ) : ForsendelseEntity {
     override val unikReferanse get() = "revurdering_forskudd_${batchId}_${tilStønadsidReferanse()}"
 
-    fun tilStønadsidReferanse(): String {
-        return Stønadsid(
+    fun tilStønadsidReferanse(): String =
+        Stønadsid(
             Stønadstype.FORSKUDD,
             Personident(barn.kravhaver),
             personidentNav,
             Saksnummer(barn.saksnummer),
         ).toReferanse()
-    }
+
     val begrunnelseVisningsnavn
         get() =
             begrunnelse.map { begrunnelse ->
