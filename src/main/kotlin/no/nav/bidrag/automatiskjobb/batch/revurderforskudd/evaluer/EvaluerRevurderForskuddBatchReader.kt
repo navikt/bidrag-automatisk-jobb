3b07f1ff4f3964e3a310ff06c4cc4a7fcf3e3358
Simen Høston <simen.naper.hoston@webstep.no>
2026-01-09 09:34:53 +0100

    Refaktorerer EvaluerRevurderForskuddBatchReader til å bruke stepScope for å lazy initialisere bønnen slik at batch parameteret blir tatt med

--- src/main/kotlin/no/nav/bidrag/automatiskjobb/batch/revurderforskudd/evaluer/EvaluerRevurderForskuddBatchReader.kt
+++ src/main/kotlin/no/nav/bidrag/automatiskjobb/batch/revurderforskudd/evaluer/EvaluerRevurderForskuddBatchReader.kt
@@ -3,29 +3,26 @@ package no.nav.bidrag.automatiskjobb.batch.revurderforskudd.evaluer
 import no.nav.bidrag.automatiskjobb.batch.BatchConfiguration.Companion.CHUNK_SIZE
 import no.nav.bidrag.automatiskjobb.persistence.entity.RevurderingForskudd
 import no.nav.bidrag.automatiskjobb.persistence.repository.RevurderForskuddRepository
-import org.springframework.batch.core.StepExecution
-import org.springframework.batch.core.annotation.BeforeStep
+import org.springframework.batch.core.configuration.annotation.StepScope
 import org.springframework.batch.item.data.RepositoryItemReader
 import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Component
 import java.time.YearMonth
 
 @Component
-class EvaluerRevurderForskuddBatchReader {
-    private var forMåned: YearMonth = YearMonth.now()
-
-    @BeforeStep
-    fun beforeStep(stepExecution: StepExecution) {
-        stepExecution.jobParameters.getString("forMåned")?.let { forMåned = YearMonth.parse(it) }
-    }
-
+class EvaluerRevurderForskuddBatchReaderClass {
     @Bean
+    @StepScope
     fun evaluerRevurderForskuddBatchReader(
         revurderForskuddRepository: RevurderForskuddRepository,
-    ): RepositoryItemReader<RevurderingForskudd> =
-        RepositoryItemReaderBuilder<RevurderingForskudd>()
+        @Value("#{jobParameters['forMåned']}") forMånedString: String?,
+    ): RepositoryItemReader<RevurderingForskudd> {
+        val forMåned = forMånedString?.let { YearMonth.parse(it) } ?: YearMonth.now()
+
+        return RepositoryItemReaderBuilder<RevurderingForskudd>()
             .name("evaluerRevurderForskuddBatchReader")
             .repository(revurderForskuddRepository)
             .methodName("findAllByForMåned")
@@ -34,4 +31,5 @@ class EvaluerRevurderForskuddBatchReader {
             .sorts(mapOf("id" to Sort.Direction.ASC))
             .saveState(false)
             .build()
+    }
 }
