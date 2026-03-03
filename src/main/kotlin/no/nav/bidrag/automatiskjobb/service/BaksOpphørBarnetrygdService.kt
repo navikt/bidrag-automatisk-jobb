28a223d9644665ca76337d8d478cf5c1558cb9b1
Magnus Rinnan <magnus.rinnan@nav.no>
2026-03-03 14:19:15 +0100

    Logikk for å lytte på topic med opphørt/redusert barnetrygd fra BA-SAK

---
+++ src/main/kotlin/no/nav/bidrag/automatiskjobb/service/BaksOpphørBarnetrygdService.kt
@@ -0,0 +1,45 @@
+package no.nav.bidrag.automatiskjobb.service
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import no.nav.bidrag.automatiskjobb.domene.BarnetrygdBisysMelding
+import no.nav.bidrag.automatiskjobb.domene.BarnetrygdEndretType
+import no.nav.bidrag.automatiskjobb.persistence.repository.BarnRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDate
+
+private val LOGGER = KotlinLogging.logger { }
+
+@Service
+class BaksOpphørBarnetrygdService(
+    private val barnRepository: BarnRepository,
+    private val oppgaveService: OppgaveService,
+) {
+    @Transactional
+    fun behandleBarnetrygdHendelse(hendelse: BarnetrygdBisysMelding) {
+        hendelse.barn.forEach { barnhendelse ->
+            val barnMedForskudd =
+                barnRepository
+                    .finnLøpendeForskuddForBarn(dato = LocalDate.now(), kravhaver = barnhendelse.ident)
+                    .firstOrNull()
+            val barnMedBidrag =
+                barnRepository
+                    .finnLøpendeBidragForBarn(dato = LocalDate.now(), kravhaver = barnhendelse.ident)
+                    .firstOrNull()
+            if ((barnMedForskudd != null || barnMedBidrag != null) &&
+                barnhendelse.årsakskode in listOf(BarnetrygdEndretType.RO, BarnetrygdEndretType.RR)
+            ) {
+                LOGGER.info {
+                    "Vedtak om reduksjon/opphør av barnetrygd mottatt for barn det løper forskudd eller bidrag for: ${barnhendelse.ident} "
+                }
+                // lag oppgave for å revurdere forskudd og bidrag for barnet
+                oppgaveService.opprettRevurderForskuddOgBidragOppgave(
+                    saksnummer = barnMedForskudd?.saksnummer ?: barnMedBidrag!!.saksnummer,
+                    mottaker = hendelse.søker,
+                    kravhaver = barnhendelse.ident,
+                    fom = barnhendelse.fom,
+                )
+            }
+        }
+    }
+}
