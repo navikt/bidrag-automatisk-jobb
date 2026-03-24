a9287e4c4f1ac7d7101e56a22afa21de5dedffff
Simen Høston <simen.naper.hoston@webstep.no>
2026-03-24 15:11:38 +0100

    Fjerner ubruke imports

--- src/main/kotlin/no/nav/bidrag/automatiskjobb/batch/revurderforskudd/revurderingslenke/RevurderingslenkeRevurderForskuddBatchConfiguration.kt
+++ src/main/kotlin/no/nav/bidrag/automatiskjobb/batch/revurderforskudd/revurderingslenke/RevurderingslenkeRevurderForskuddBatchConfiguration.kt
@@ -13,10 +13,8 @@ import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
 import org.springframework.batch.item.data.RepositoryItemReader
 import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.task.TaskExecutor
 import org.springframework.data.domain.Sort
 import org.springframework.transaction.PlatformTransactionManager
 
