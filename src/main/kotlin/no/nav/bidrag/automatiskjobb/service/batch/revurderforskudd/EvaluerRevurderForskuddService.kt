8a08d3af1a7ffa1a3c4f241ba207a6fa2459d3e7
Simen Høston <simen.naper.hoston@webstep.no>
2026-04-13 14:29:57 +0200

    ktlint

--- src/main/kotlin/no/nav/bidrag/automatiskjobb/service/batch/revurderforskudd/EvaluerRevurderForskuddService.kt
+++ src/main/kotlin/no/nav/bidrag/automatiskjobb/service/batch/revurderforskudd/EvaluerRevurderForskuddService.kt
@@ -354,21 +354,23 @@ class EvaluerRevurderForskuddService(
                 stønadsendringer,
                 grunnlagsliste,
             )
-        val vedtakId = if (simuler) {
-            null
-        } else {
-            try {
-                bidragVedtakConsumer.opprettEllerOppdaterVedtaksforslag(opprettVedtakRequestDto)
-            } catch (e: Exception) {
-                LOGGER.error(e) {
-                    "Feil ved oppretting av vedtaksforslag for revurdering av forskudd for sak ${revurderingForskudd.saksnummer}"
+        val vedtakId =
+            if (simuler) {
+                null
+            } else {
+                try {
+                    bidragVedtakConsumer.opprettEllerOppdaterVedtaksforslag(opprettVedtakRequestDto)
+                } catch (e: Exception) {
+                    LOGGER.error(e) {
+                        "Feil ved oppretting av vedtaksforslag for revurdering av forskudd for sak " +
+                            revurderingForskudd.saksnummer
+                    }
+                    revurderingForskudd.behandlingstype = Behandlingstype.FEILET
+                    revurderingForskudd.status = Status.FEILET
+                    revurderingForskudd.begrunnelse = listOf("FEIL_VED_OPPRETTING_AV_VEDTAKSFORSLAG: ${e.message}")
+                    return revurderingForskudd
                 }
-                revurderingForskudd.behandlingstype = Behandlingstype.FEILET
-                revurderingForskudd.status = Status.FEILET
-                revurderingForskudd.begrunnelse = listOf("FEIL_VED_OPPRETTING_AV_VEDTAKSFORSLAG: ${e.message}")
-                return revurderingForskudd
             }
-        }
         revurderingForskudd.vedtak = vedtakId
         revurderingForskudd.status = if (simuler) Status.SIMULERT else Status.BEHANDLET
         revurderingForskudd.behandlingstype = Behandlingstype.FATTET_FORSLAG
