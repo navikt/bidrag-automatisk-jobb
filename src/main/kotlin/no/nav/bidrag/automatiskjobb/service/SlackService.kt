20bcc8b18ba9c983092beb00f5657fc2c5d88e9c
Simen Høston <simen.naper.hoston@webstep.no>
2026-04-14 14:22:37 +0200

    Legger til SlackService for å sende meldinger til Slack-kanal

---
+++ src/main/kotlin/no/nav/bidrag/automatiskjobb/service/SlackService.kt
@@ -0,0 +1,19 @@
+package no.nav.bidrag.automatiskjobb.service
+
+import com.slack.api.Slack
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+
+@Service
+class SlackService(
+    @param:Value("\${BIDRAG_BOT_SLACK_OAUTH_TOKEN}") private val oauthToken: String,
+    @param:Value("\${SLACK_CHANNEL_ID}") private val channel: String,
+) {
+
+    fun sendMelding(message: String) {
+        Slack.getInstance().methods(oauthToken).chatPostMessage {
+            it.channel(channel)
+                .text(message)
+        }
+    }
+}
