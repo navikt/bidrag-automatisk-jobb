f5c8237f9e06fe539eb922a4b07fb058b5615dac
Simen Høston <simen.naper.hoston@webstep.no>
2026-04-14 14:22:09 +0200

    Legger til SlackController for å håndtere Slack-meldinger og inkluderer slack-api-client avhengighet i pom.xml

---
+++ src/main/kotlin/no/nav/bidrag/automatiskjobb/controller/SlackController.kt
@@ -0,0 +1,19 @@
+package no.nav.bidrag.automatiskjobb.controller
+
+import no.nav.bidrag.automatiskjobb.service.SlackService
+import no.nav.security.token.support.core.api.Protected
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@Protected
+@RestController
+class SlackController(
+    private val slackService: SlackService
+) {
+
+    @PostMapping("/slack/melding")
+    fun sendMelding(@RequestBody melding: String) {
+        slackService.sendMelding(melding)
+    }
+}
