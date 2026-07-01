bf992e7836d4cbb61f22f04de5e3ebc80d578b90
Simen Høston <simen.naper.hoston@webstep.no>
2026-07-01 12:52:25 +0200

    Fjerner ubrukt logger

--- src/main/kotlin/no/nav/bidrag/automatiskjobb/batch/indeksregulering/bidrag/opprett/OpprettIndeksreguleringBidragBatchWriter.kt
+++ src/main/kotlin/no/nav/bidrag/automatiskjobb/batch/indeksregulering/bidrag/opprett/OpprettIndeksreguleringBidragBatchWriter.kt
@@ -1,13 +1,10 @@
 package no.nav.bidrag.automatiskjobb.batch.indeksregulering.bidrag.opprett
 
-import io.github.oshai.kotlinlogging.KotlinLogging
 import no.nav.bidrag.automatiskjobb.persistence.entity.Indeksregulering
 import no.nav.bidrag.automatiskjobb.persistence.repository.IndeksreguleringRepository
 import org.springframework.batch.infrastructure.item.Chunk
 import org.springframework.batch.infrastructure.item.ItemWriter
 import org.springframework.stereotype.Component
-
-private val LOGGER = KotlinLogging.logger { }
 
 @Component
 class OpprettIndeksreguleringBidragBatchWriter(
