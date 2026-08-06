0fda2569e0095fdfdd1d31d18d0a7c9571b1fadc
Simen Høston <simen.naper.hoston@webstep.no>
2026-08-06 11:35:38 +0200

    Transactional

--- src/main/kotlin/no/nav/bidrag/automatiskjobb/service/batch/indeksregulering/SlettIndeksreguleringService.kt
+++ src/main/kotlin/no/nav/bidrag/automatiskjobb/service/batch/indeksregulering/SlettIndeksreguleringService.kt
@@ -1,5 +1,6 @@
 package no.nav.bidrag.automatiskjobb.service.batch.indeksregulering
 
+import jakarta.transaction.Transactional
 import no.nav.bidrag.automatiskjobb.persistence.repository.IndeksreguleringRepository
 import no.nav.bidrag.commons.util.secureLogger
 import org.springframework.stereotype.Service
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service
 class SlettIndeksreguleringService(
     private val indeksreguleringRepository: IndeksreguleringRepository,
 ) {
+    @Transactional
     fun slettIndeksreguleringForÅr(år: Int) {
         secureLogger.info { "Sletter alle indeksreguleringer for år $år" }
         indeksreguleringRepository.deleteAllByÅr(år)
